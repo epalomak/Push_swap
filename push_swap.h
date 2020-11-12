55954158b714fde2635e8ca2dfc4b38cf3d37608
Eino Palomäki <eino.palomaki@icloud.com>
2020-11-12 14:43:20 +0200

    updated header

--- push_swap.h
+++ push_swap.h
@@ -6,12 +6,15 @@
 /*   By: epalomak <epalomak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 13:18:36 by epalomak          #+#    #+#             */
-/*   Updated: 2020/11/11 12:06:08 by epalomak         ###   ########.fr       */
+/*   Updated: 2020/11/12 14:41:41 by epalomak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+# define BUFF_SIZE 42
+# define MAX_FD 4864
 
 # include "../libft/libft.h"
 # include "../libft/printf/headers.h"
@@ -30,7 +33,13 @@ typedef struct	s_ps
 	
 }				t_ps;
 
+
+void	swap(t_ps *ps, char *cmd);
+void	push(t_ps *ps, char *cmd);
+void	rotate(t_ps *ps, char *cmd);
+void	rev_rotate(t_ps *ps, char *cmd);
 void	display_errors();
 int		*get_numbers(t_ps *ps, char **av);
+int		get_next_line(const int fd, char **line);
 
 #endif
