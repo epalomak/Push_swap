14be084b5e40dc2839122dba0a0719b18311a51c
Eino Palomäki <eino.palomaki@icloud.com>
2020-11-12 14:42:53 +0200

    commands can be read and base functions to all commands

---
+++ src/get_next_line.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epalomak <epalomak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/07 14:07:09 by epalomak          #+#    #+#             */
+/*   Updated: 2020/11/11 17:51:07 by epalomak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+static	int	ft_line(char **src, char **line)
+{
+	int		i;
+	char	*next;
+
+	i = 0;
+	while (src[0][i] != '\n' && src[0][i] != '\0')
+		i++;
+	if (src[0][i] == '\n')
+	{
+		*line = ft_strsub(src[0], 0, i);
+		next = ft_strdup(src[0] + (i + 1));
+		free(src[0]);
+		src[0] = next;
+	}
+	else if (src[0][i] == '\0')
+	{
+		*line = ft_strdup(src[0]);
+		ft_strdel(&src[0]);
+	}
+	return (1);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	int			ret;
+	char		buf[BUFF_SIZE + 1];
+	static char	*str[MAX_FD];
+	char		*tmp;
+
+	if (!line || fd < 0 || fd > MAX_FD)
+		return (-1);
+	if (str[fd] == NULL)
+		if (!(str[fd] = ft_strnew(0)))
+			return (-1);
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		tmp = ft_strjoin(str[fd], buf);
+		free(str[fd]);
+		str[fd] = tmp;
+		if (ft_strchr(str[fd], '\n'))
+			break ;
+	}
+	if (ret < 0 || str[fd] == NULL)
+		return (-1);
+	if (ret == 0 && str[fd][0] == '\0')
+		return (0);
+	return (ft_line(&str[fd], line));
+}
---
+++ src/handle_commands.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_commands.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epalomak <epalomak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/12 12:07:05 by epalomak          #+#    #+#             */
+/*   Updated: 2020/11/12 13:37:32 by epalomak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void	swap(t_ps *ps, char *cmd)
+{
+
+}
+
+void	push(t_ps *ps, char *cmd)
+{
+
+}
+
+void	rotate(t_ps *ps, char *cmd)
+{
+
+}
+
+void	rev_rotate(t_ps *ps, char *cmd)
+{
+	
+}
